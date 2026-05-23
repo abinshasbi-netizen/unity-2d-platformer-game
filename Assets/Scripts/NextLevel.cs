26c1fd54e78700f37b6bd3783564237c442cee8c
Abin <abinshasbi@gmail.com>
2026-05-23 21:29:07 +0530

    Implemented scene management and level progression systems
    Added multi-scene gameplay flow including main menu integration, level transition handling, scene loading systems, restart functionality, and progression management between gameplay levels to establish a complete game structure.

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        GameManager.Instance.LoadNextLevel();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
